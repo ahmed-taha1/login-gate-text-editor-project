7905decfa1001f03896c41a0564128465ee2d177
Ahmed Taha <bjabotaha@yahoo.com>
2022-04-27 11:03:50 +0200

    initial commit

---
+++ text_editor.cpp
@@ -0,0 +1,42 @@
+// FCAI – Programming 1 – 2022 - Assignment 4
+// Program Name: text_editor.cpp
+// Program Description: text editor program
+// Last Modification Date: 27/4/2022
+// Author1 and ID and Group: Ahmed Mohamed Taha - 20210033 - Group A - S8
+// Author2 and ID and Group: Doaa Mahdy Mohamed - 20210128 - Group A - S8
+// Author3 and ID and Group: Omar Ayman Saad    - 20210261 - Group A - S8
+// Teaching Assistant: Abdulrahman Abdulmonem
+// Purpose: training on struct and reading or writing files
+
+// libraries
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <stdlib.h>
+#include <string>
+#include <array>
+
+// initialize standard cpp
+using namespace std;
+
+// functions
+
+
+
+
+
+
+
+
+
+
+
+
+// main function
+int main() {
+
+
+
+}
